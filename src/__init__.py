21b6a02fb88a2d1536eb813628879c582b82f1da
DerGut <j.stoneman@t-online.de>
2016-06-11 23:13:07 +0200

    request_func

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,4 @@ import json
 
 with open('res/credentials.json') as f:
 	credentials = json.load(f)['credentials']
-	
-	url  = credentials['url']
-	api_key = credentials['apikey']
 
--- src/extractArticle.py
+++ src/extractArticle.py
@@ -6,4 +6,32 @@ Extract Article
 
 """
 
-# news_sites = ['http://www.nytimes.com', 'http://foxnews.com', ]
+import requests
+import json
+
+from __init__ import credentials
+
+
+def query(keywords, return_values, outputMode='json', start='now-30d', end='now', count='5'):
+	"""
+
+	Args:
+		keywords: A list of keywords
+	
+		return_values: A list of values to return
+	"""
+	url = credentials['url'] + '/data/GetNews?'
+	url += 'outputMode=' + outputMode
+	url += '&start=' + start
+	url += '&end=' + end
+	url += '&count=' + count
+	url += '&q.enriched.url.enrichedTitle.keywords.keyword.text=A[' + '^'.join(keywords) + ']'
+	url += '&return=' + ','.join(return_values)
+	url += '&apikey=' + credentials['apikey']
+	return url
+
+# .json()
+q = query(['paintings'], ['enriched.url.url', 'enriched.url.title'])
+r = requests.get(q)
+
+print(r.text)
